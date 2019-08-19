3998b9c7cdaf2599f485c8a2829c821d5bf08dca
Paulo <paulomelojunior@icloud.com>
2019-08-19 14:58:37 -0300

    Removing fonts

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function () {
     setTimeout(() => {
         document.querySelector('body').classList.remove('loading');
-    }, 500);
+    }, 1000);
 });
---
+++ assets/js/menu.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('button');
+const menu = document.querySelector('nav');
+const main = document.querySelector('main');
+
+function toggleMenu() {
+    menu.classList.toggle('closed');
+    menu.classList.toggle('opened');
+    button.classList.toggle('inverted');
+}
+
+button.addEventListener('click', function (e) {
+    toggleMenu();
+    e.stopPropagation();
+})
+
+main.addEventListener('click', function (e) {
+    if (menu.classList.contains('opened')) {
+        toggleMenu()
+    }
+})
