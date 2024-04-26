537559901cba0541adc59b5589b60896cf3f4439
paulomelojunior <14110514+paulomelojunior@users.noreply.github.com>
2024-04-26 03:26:54 -0300

    New theme

---
+++ assets/scripts/menu.js
@@ -0,0 +1,28 @@
+const navList = document.querySelector('.menu')
+const navItems = {
+    github: {
+        label: 'GitHub',
+        url: 'https://www.github.com/paulomelojunior',
+    },
+    instagram: {
+        label: 'Instagram',
+        url: 'https://www.instagram.com/paulomelojunior',
+    },
+    xCorp: {
+        label: 'X Corp.',
+        url: 'https://www.x.com/hackyoto',
+    },
+}
+
+const navValues = Object.values(navItems)
+
+navValues.forEach(element => {
+    const navItem = `
+        <li class="menu_item">
+            <a target="_blank" class="menu_link" rel="noopener noreferrer" href="${element.url}">
+                ${element.label}
+            </a>
+        </div>`
+        
+    navList.insertAdjacentHTML('beforeend', navItem)
+})
