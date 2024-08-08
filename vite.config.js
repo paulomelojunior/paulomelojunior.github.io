fced0ec0070e5ef5b743bf86dccfc90d62c255ba
paulomelojunior <14110514+paulomelojunior@users.noreply.github.com>
2024-08-08 00:47:57 -0300

    Bitcoin

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,12 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                bitcoin: resolve(__dirname, 'btc.html'),
                 404: resolve(__dirname, '404.html'),
             },
         },
     },
+    server: {
+        host: true
+    }
 })
