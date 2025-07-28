df97e4d31f6edf6ab99f1cc05b5093425122cd0a
paulomelojunior <paulomelojunior@icloud.com>
2025-07-28 12:38:57 -0300

    Copyright background mobile color

--- src/components/menu/container.ts
+++ src/components/menu/container.ts
@@ -117,7 +117,7 @@ export class MenuContainer extends ThemeMixin(LitElement) {
             </ul>
             <div
               id="copy"
-              class="absolute flex h-12 w-full items-center justify-center bg-stone-300 px-5 font-mono text-[.625rem] font-semibold uppercase xl:hidden dark:bg-zinc-900"
+              class="absolute flex h-12 w-full items-center justify-center bg-stone-300 px-5 font-mono text-[.625rem] font-semibold uppercase xl:hidden dark:bg-zinc-950"
             >
               <span> Copyright 2025 Paulo Melo Jr. </span>
             </div>
