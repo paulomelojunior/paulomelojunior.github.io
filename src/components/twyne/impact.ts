49f034813625348efc1352d821eb54fc2941588b
paulomelojunior <paulomelojunior@icloud.com>
2025-08-06 22:03:25 -0300

    Numbers

--- src/components/twyne/impact.ts
+++ src/components/twyne/impact.ts
@@ -26,27 +26,57 @@ export class TwyneImpact extends LitElement {
       returnObjects: true,
     }) as string[]
     return html`
-      <div
-        id="impact"
-        class="container grid grid-cols-2 items-baseline rounded-b-[2rem] px-24 pt-32 2xl:px-32"
-      >
-        <h2
-          class="text-[2.5rem] leading-none tracking-[-.02em] 2xl:text-[3rem] dark:text-zinc-200"
+      <div class="grid gap-24 2xl:gap-32">
+        <div
+          id="impact"
+          class="container grid grid-cols-2 items-baseline rounded-b-[2rem] px-24 pt-32 2xl:px-32"
         >
-          ${unsafeHTML(i18next.t('twyne.impact.t1'))}
-        </h2>
-        <ul
-          class="text-[1rem] leading-[2.25] 2xl:text-[1.25rem] dark:text-zinc-200"
-        >
-          ${items.map(
-            (item) =>
-              html`<li
-                class="flex items-center gap-5 text-center before:text-[1rem] before:text-brand-400 before:content-['✦']"
-              >
-                ${item}
-              </li>`
-          )}
-        </ul>
+          <h2
+            class="text-[2.5rem] leading-none tracking-[-.02em] 2xl:text-[3rem] dark:text-zinc-200"
+          >
+            ${unsafeHTML(i18next.t('twyne.impact.t1'))}
+          </h2>
+          <ul
+            class="text-[1rem] leading-[2.25] 2xl:text-[1.25rem] dark:text-zinc-200"
+          >
+            ${items.map(
+              (item) =>
+                html`<li
+                  class="flex items-center gap-5 text-center before:text-[1rem] before:text-brand-400 before:content-['✦']"
+                >
+                  ${item}
+                </li>`
+            )}
+          </ul>
+        </div>
+        <div class="container">
+          <div class="grid grid-cols-3 px-16 2xl:px-24">
+            <div class="grid gap-2 px-8">
+              <h3 class="text-[2rem] leading-none dark:text-zinc-200">
+                ${i18next.t('twyne.numbers.t1')}
+              </h3>
+              <p class="text-[1rem] leading-none">
+                ${i18next.t('twyne.numbers.p1')}
+              </p>
+            </div>
+            <div class="grid gap-2 border-x border-zinc-800 px-8">
+              <h3 class="text-[2rem] leading-none dark:text-zinc-200">
+                ${i18next.t('twyne.numbers.t2')}
+              </h3>
+              <p class="text-[1rem] leading-none">
+                ${i18next.t('twyne.numbers.p2')}
+              </p>
+            </div>
+            <div class="grid gap-2 px-8">
+              <h3 class="text-[2rem] leading-none dark:text-zinc-200">
+                ${i18next.t('twyne.numbers.t3')}
+              </h3>
+              <p class="text-[1rem] leading-none">
+                ${i18next.t('twyne.numbers.p3')}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     `
   }
